2c413e48037a904d32f5997057cc7dcdf7da4b70
unknown <morenorubionina@gmail.com>
2024-11-25 17:14:09 +0100

    app corrections #3

--- staff/nina-moreno/app.12/logic/helper/validate.js
+++ staff/nina-moreno/app.12/logic/helper/validate.js
@@ -3,17 +3,27 @@ var validate = (function () {
     if (typeof username !== "string") throw new Error("invalid username type");
     if (username.length < 4) throw new Error("invalid username length");
   }
+
   function validatePassword(password) {
     if (typeof password !== "string") throw new Error("invalid password type");
     if (password.length < 8) throw new Error("invalid password length");
   }
+
   function validateName(name) {
     if (typeof name !== "string") throw new Error("invalid name type");
     if (name.length < 1) throw new Error("invalid name length");
   }
+
   function validateEmail(email) {
     if (typeof email !== "string") throw new Error("invalid email type");
     if (email.length < 6) throw new Error("invalid email length");
+  }
+
+  function validateId(id, explain) {
+    if (typeof id !== "string")
+      throw new Error("invalid " + (explain || "id") + " type");
+    if (id.length < 10)
+      throw new Error("invalid " + (explain || "id") + " length");
   }
 
   return {
@@ -21,5 +31,6 @@ var validate = (function () {
     password: validatePassword,
     name: validateName,
     email: validateEmail,
+    id: validateId,
   };
 })();
