c0534e4bd82b5033fb21611e2c7b0bb7f42ef72c
unknown <morenorubionina@gmail.com>
2024-12-01 17:42:02 +0100

    save new users in bbdd isers.json #3

--- staff/nina-moreno/playground/web-server/data/localStorage.js
+++ staff/nina-moreno/playground/web-server/data/localStorage.js
@@ -1,6 +1,17 @@
+const fs = require("fs");
+
 const localStorage = {
-  users:
-    '[{"id":"abc123","name":"Peter Pan","email":"peter@pan.com","username":"peterpan","password":"123123123"}]',
+  get users() {
+    // ex: const users = JSON.parse(localStorage.users)
+    const users = fs.readFileSync("./data/users.json", "utf8");
+
+    return users;
+  },
+
+  set users(users) {
+    // ex: localStorage.users = JSON.stringify(users)
+    fs.writeFileSync("./data/users.json", users);
+  },
 };
 
 module.exports = localStorage;
